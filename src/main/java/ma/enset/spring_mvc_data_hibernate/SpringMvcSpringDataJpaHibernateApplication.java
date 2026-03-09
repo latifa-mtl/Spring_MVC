e5c05f791692a8d43a86cf1e94d9d5c7f9d5c420
Latifa Moutawakil <latefamoutawakil@gmail.com>
2026-03-09 03:19:35 +0000

    Initial commit

---
+++ src/main/java/ma/enset/spring_mvc_data_hibernate/SpringMvcSpringDataJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.spring_mvc_data_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSpringDataJpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSpringDataJpaHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/spring_mvc_data_hibernate/SpringMvcSpringDataJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.spring_mvc_data_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSpringDataJpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
